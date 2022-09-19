bfdb5726d868bb5f7241528141c72e4a29f94c74
MrPhoenixYear <96641362+MrPhoenixYear@users.noreply.github.com>
2022-09-19 08:17:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "yh星辰大海",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o74V76DwEpt_bzFk-qRWrpQTVs6A",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "yUZSwIJO6EfoyHWP0jvJqv4XVr21lr2dbPuWLicLFYA",
       // 所在省份
